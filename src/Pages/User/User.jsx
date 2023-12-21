19c7b9a56046e4dd508c9f86c08c4690253306dd
boburbekburkhonov <zafaroviccc@gmail.com>
2023-12-21 11:23:05 +0500

    feat: .

--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./User.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import MailIcon from '@mui/icons-material/Mail';
 import menuBar from "../../assets/images/menu-bar.png";
 import dashboard from "../../assets/images/dashboard.png";
 import dashboardBlack from "../../assets/images/dashboard-black.png";
@@ -23,14 +24,97 @@ import UserLastData from "../UserLastData/UserLastData";
 import UserLastDataNews from "../UserLastDataNews/UserLastDataNews";
 import { api } from "../Api/Api";
 import { useState } from "react";
+import { Badge } from "@mui/material";
+import axios from "axios";
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 const User = () => {
+  const [countNotification, setCountNotification] = useState(0);
+  const [notificationMessage, setNotificationMessage] = useState([]);
   const [balanceOrg, setBalanceOrg] = useState([]);
   const token = window.localStorage.getItem("accessToken");
   const role = window.localStorage.getItem("role");
   const username = window.localStorage.getItem("username");
   const location = useLocation();
   const navigate = useNavigate();
+
+  // ! CUSTOM FETCH
+  const customFetch = axios.create({
+    baseURL: api,
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+
+  // ! ADD HEADER TOKEN
+  customFetch.interceptors.request.use(
+    async (config) => {
+      const token = window.localStorage.getItem("accessToken");
+      if (token) {
+        config.headers["Authorization"] = ` bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  // ! REFRESH TOKEN
+  const refreshToken = async () => {
+    try {
+      const requestToken = await fetch(`${api}/auth/signin`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          username: window.localStorage.getItem("username"),
+          password: window.localStorage.getItem("password"),
+        }),
+      });
+
+      const responToken = await requestToken.json();
+      return responToken.data.accessToken;
+    } catch (e) {
+      console.log("refreshToken", "Error", e);
+    }
+  };
+
+  // ! GET ACCESS TOKEN
+  customFetch.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async function (error) {
+      const originalRequest = error.config;
+      if (
+        (error.response?.status === 403 && !originalRequest._retry) ||
+        (error.response?.status === 401 && !originalRequest._retry)
+      ) {
+        originalRequest._retry = true;
+
+        const resp = await refreshToken();
+
+        const access_token = resp;
+
+        window.localStorage.setItem("accessToken", access_token);
+
+        customFetch.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${access_token}`;
+        return customFetch(originalRequest);
+      }
+      return Promise.reject(error);
+    }
+  );
 
   useEffect(() => {
     if (!token) {
@@ -53,6 +137,16 @@ const User = () => {
 
       balansOrgName();
     }
+
+    // ! NOTIFICATION
+    customFetch
+      .get(`/user-messages/getAllUserMessgesCount`)
+      .then((data) => setCountNotification(data.data.data));
+
+      // ! NOTIFICATION MESSAGE
+    customFetch
+    .get(`/user-messages/getAllUserMessages`)
+    .then((data) => setNotificationMessage(data.data.data));
   }, []);
 
   function logoutFunction() {
@@ -65,9 +159,64 @@ const User = () => {
     window.location.reload();
   }
 
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
   return (
     <HelmetProvider>
       <div className="admin-wrapper">
+        //! Modal
+        <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered quiz-modal-width">
+            <div className="modal-content quiz-modal-height">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body m-auto">
+                <Box sx={{ maxWidth: 400 }}>
+                  <Stepper activeStep={activeStep} orientation="vertical">
+                    {notificationMessage.map((step, index) => (
+                      <Step key={index + 1}>
+                        <StepLabel
+                          optional={
+                            index === 2 ? (
+                              <Typography variant="caption">Last step</Typography>
+                            ) : null
+                          }
+                        >
+                          {/* {index + 1} */}
+                        </StepLabel>
+                        <StepContent>
+                          <Typography>{step.message}</Typography>
+                          <Box sx={{ mb: 2 }}>
+                            <div>
+                              <Button
+                                variant="contained"
+                                onClick={handleNext}
+                                sx={{ mt: 1, mr: 1 }}
+                              >
+                                {index === notificationMessage.length - 1 ? 'Finish' : 'Continue'}
+                              </Button>
+                            </div>
+                          </Box>
+                        </StepContent>
+                      </Step>
+                    ))}
+                  </Stepper>
+                </Box>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="sidebar">
           <div className="logo-details">
             <img
@@ -239,7 +388,7 @@ const User = () => {
           </ul>
         </div>
         <header className="home-section-header">
-          <div className="container-fluid py-3">
+          <div className="container-fluid pb-3">
             <div className="dropdown text-end d-flex align-items-center justify-content-between">
               <h2 className="admin-page-heading m-0">Smart Well</h2>
 
@@ -252,6 +401,9 @@ const User = () => {
                   height={30}
                 />
                 <span className="mx-2">{username}</span>
+                <Badge className="notification-message cursor-pointer me-3" color="warning" badgeContent={countNotification} data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                  <MailIcon />
+                </Badge>
               </div>
             </div>
           </div>
